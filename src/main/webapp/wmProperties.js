c2a866de83df09b505d86cec223d0f83688bbd81
root <root@be1c98905f37>
2017-06-20 00:57:38 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_mybaUpnSgJ",
+  "homePage" : "Main",
+  "name" : "Auto_mybaUpnSgJ",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
